d99139b3c244069c0a9bfc9ab0f26eefda3f926b
DiptaPal <diptapal1998@gmail.com>
2022-11-12 19:33:47 +0600

    filtering is added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,21 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Service from '../Service/Service';
 import { Link } from 'react-router-dom';
 
 
 const Services = () => {
     const [services, setServices] = useState([])
+
+    const [isAsc, setIsAsc] = useState(true)
+    const searchRef = useRef();
+    const [search, setSearch] = useState('')
+
     useEffect(() => {
-        fetch('https://genius-car-server-sand-ten.vercel.app/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    }, [])
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [isAsc, search])
+
+    const handleSearch = () =>{
+        setSearch(searchRef.current.value)
+    }
+
     return (
         <div className='my-20 px-6'>
             <div className='text-center mb-10 flex flex-col gap-6'>
                 <p className='text-2xl font-bold text-red-500'>Service</p>
                 <h1 className='text-5xl font-bold'>Our Service Area</h1>
                 <p className='text-xl'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+            </div>
+            <div className='flex flex-col justify-between items-end my-10'>
+                <div>
+                    <input ref={searchRef} className='border outline-none py-2 rounded-md border-blue-300' type="text" />
+                    <button onClick={handleSearch} className='btn btn-outline btn-accent'>Search</button>
+                </div>
+                <br />
+                <button className='btn btn-active btn-primary' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'Desc' : 'Asc'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
